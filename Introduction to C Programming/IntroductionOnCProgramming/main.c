f252ac05868fc9a0bf7c172f2ca54db5c553ae0c
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-10-15 00:02:48 +0300

    update

--- Introduction to C Programming/IntroductionOnCProgramming/main.c
+++ Introduction to C Programming/IntroductionOnCProgramming/main.c
@@ -8,11 +8,15 @@
 
 int main() {
     double squareRoot;
+    int sequence;
     PrintHelloC();
     PrintMyName();
     PrintNumbers();
     PrintFirstNameAndLastName("Ivaylo", "Ivanov");
     PrintSquareRoot();
+    PrintASequence(10);
+    printf("Read number from console for print sequence of numbers. Input number: %d", &sequence);
+    PrintASequence(sequence);
     return (EXIT_SUCCESS);
 }
 
