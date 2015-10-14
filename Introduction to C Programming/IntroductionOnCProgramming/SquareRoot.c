4ee2d1947456beaa5650fe0e4cd934d36064b53b
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-10-15 00:02:42 +0300

    update

--- Introduction to C Programming/IntroductionOnCProgramming/SquareRoot.c
+++ Introduction to C Programming/IntroductionOnCProgramming/SquareRoot.c
@@ -7,9 +7,10 @@ int PrintSquareRoot()
     printf("Square root of 12345 is %lf\n", sqrt(12345));
     return (EXIT_SUCCESS);
 }
-
+/*
 int PrintSquareRootFromConsole(double squareRoot)
 {    
     printf("Square root of %lf is %lf\n", squareRoot, sqrt(squareRoot));    
     return(EXIT_SUCCESS);
 }
+*/
