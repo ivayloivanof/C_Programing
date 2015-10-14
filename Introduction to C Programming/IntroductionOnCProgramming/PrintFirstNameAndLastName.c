85a56cae8c5587e1d6dece45c489f47ee0ac3b5f
ivayloivanof <ivaylo.ivanof@gmail.com>
2015-10-14 23:15:14 +0300

    add print first and last name file

---
+++ Introduction to C Programming/IntroductionOnCProgramming/PrintFirstNameAndLastName.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void PrintFirstNameAndLastName(char firstName[], char lastName[] )
+{
+    printf("My first name is: %s\n", firstName);
+    printf("My lastname is: %s\n", lastName);
+}
